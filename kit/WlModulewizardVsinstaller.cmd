df9d3437a29ad7dd5beef42ec3e2614000cf1771
Wayne (build machine) <w5xd@writelog.com>
2026-01-07 00:14:12 +0000

    vsix gui fixes

--- kit/WlModulewizardVsinstaller.cmd
+++ kit/WlModulewizardVsinstaller.cmd
@@ -8,5 +8,7 @@
 @set vsix=%vsix:\=\\%
 @set contents={  "version": "1.0",  "components": [], "extensions": ["%vsix%" ] }
 @set vsconfig=%~dp0wlwizard.vsconfig
+@echo If the WL12 Module Wizard is already installed, you must use Visual Studio Installer to manually remove it before this one will work!
+@pause
 @echo %contents% > "%vsconfig%"
 "C:\Program Files (x86)\Microsoft Visual Studio\Installer\setup.exe" modify --installPath "%installPath%" --config "%vsconfig%" --allowUnsignedExtensions
